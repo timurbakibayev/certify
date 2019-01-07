6e372ad31dd3ef79ff45711804a2481e6dbadb6d
timurbakibayev <timurbakibayev@gmail.com>
2019-01-07 21:54:11 +0600

    model of certificate

--- certify/cert/models.py
+++ certify/cert/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Certificate(models.Model):
+    name = models.CharField(max_length=1000, default="Ivan Pupkin")
+    course = models.CharField(max_length=1000, default="Python for Data Science")
+    date = models.DateField(default="2019-01-01")
+    mark = models.CharField(max_length=1000, default="Excellent")
+    permanent_link = models.CharField(max_length=1000, default="AUTO")
+
+    def __str__(self):
+        return self.name + " " + self.course

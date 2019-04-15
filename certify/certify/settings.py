99a2b98667d7b323a9beaf822bc4c2542d39b541
timurbakibayev <timurbakibayev@gmail.com>
2019-04-15 15:53:50 +0600

    80% zoom and debug=False

--- certify/certify/settings.py
+++ certify/certify/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(#dfj^6$=h3ynxm74w5hwk+b*%!&)(ay$y&_f6*c=*o5nao6*4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [r'*']
 
